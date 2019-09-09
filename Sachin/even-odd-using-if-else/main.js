10fd8e2d4b798fdbcbe07d6cc0e2db787d18bc6c
Sachin <ghugesachin24@gmail.com>
2019-09-09 22:30:32 +0530

    even odd using if
    - creted program using if for checking number is an even or odd number.

---
+++ Sachin/even-odd-using-if-else/main.js
@@ -0,0 +1,11 @@
+/*Find out even odd number.*/
+var num=25;
+var displayNumber =  "Given Number is Odd";
+ 
+ if ( num % 2  === 0 ) {
+	 displayNumber =  "Given Number is Even" ;
+ } 
+ 
+ document.write(displayNumber);
+ 
+ 
