f017bcb16f7006aa5bc5dec1055569830fcc4ccd
Aditi Astonkar <aditi.astonkar@gmail.com>
2019-09-26 22:28:13 +0530

    Merge branch 'assignments' of https://github.com/sachinghuge24/full_stack_course into assignments

---
+++ Aditi/javascript/switch-case/assignment28augcase.js
@@ -0,0 +1,18 @@
+/*wAp to check given no. is even or odd with switch case?*/
+var a=11;
+var even;
+var odd;
+var result;
+
+
+switch(a%2)
+{
+case 0:
+document.write("number is even");
+break;
+default:
+document.write("number is odd");
+}
+
+
+
