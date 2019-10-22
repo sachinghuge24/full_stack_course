af0da9a2ba3a8f09af95b34ffaedc6d000a0fb57
Bhagyashri Savant <52212285+BhagyashriSSavant@users.noreply.github.com>
2019-10-22 22:05:16 +0530

    Commited with the given changes
    add { and space wherever required.

--- Bhagyashri/Javascript/Second largest no in array/secondlargest_aaray.js
+++ Bhagyashri/Javascript/Second largest no in array/secondlargest_aaray.js
@@ -5,16 +5,14 @@ var myarray=[10,20,30,200,70];
 document.write("MY Array is " +myarray);
 var first_largest=myarray[0];
 var second_largest=myarray[0];
-for(var i=0;i<myarray.length;i++)
-{
-	if(myarray[i]>=first_largest)
-   {
-	second_largest=first_largest;
-    first_largest=myarray[i];
+for(var i=0;i<myarray.length;i++) {
+	if(myarray[i]>=first_largest) {
+		second_largest = first_largest;
+		first_largest = myarray[i];
 	}	
-else if(myarray[i]>second_largest && myarray[i]<first_largest)
-	second_largest=myarray[i];
-
+	else if ( myarray[i]>second_largest && myarray[i]<first_largest){
+		second_largest = myarray[i];
+	}
 }
 document.write('</br>');
 document.write('</br>');
