912b4a8fe5c4c6e1b612f2223accf9e034af2215
Aditi Astonkar <aditi.astonkar@gmail.com>
2019-10-22 19:43:57 +0530

    completed
    we can limit the the number of series weather we have to find till.

---
+++ Aditi/javascript/febonesis series/febonesis.js
@@ -0,0 +1,14 @@
+/*program for febonesis series*/
+var var1=0;
+var var2=1;
+var var3;
+var num=prompt("enter the limit to generate fibonacci no",0);
+document.write(var1+"</br>");
+document.write(var2+"</br>");
+for(var i=3;i<=num;i++)
+{
+	var3=var1+var2;
+	var1=var2;
+	var2=var3;
+	document.write(var3+"</br>");
+}
